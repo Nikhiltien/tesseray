ee9aa2b10a475832bffd7bbed3151312e71d9f7a
nikhiltien <nikhil17tn@gmail.com>
2023-05-16 23:17:54 -0400

    evaluation_drop

--- tesseray/blunder_analysis.py
+++ tesseray/blunder_analysis.py
@@ -4,37 +4,41 @@ import os
 import logging
 from multiprocessing import Pool, cpu_count
 
-logging.basicConfig(filename='blunder_analysis.log', level=logging.INFO, format='%(message)s')
+os.makedirs('logs', exist_ok=True)
+
+logging.basicConfig(filename='logs/blunder_analysis.log', level=logging.INFO, format='%(message)s')
 
 def analyze_game(game_info):
-    stockfish_path, blunder_threshold, game_info = game_info
+    stockfish_path, blunder_threshold, game_info, blunder_definition = game_info
     file, game, game_num = game_info
-    # Initialize the chess engine
     engine = chess.engine.SimpleEngine.popen_uci(stockfish_path)
 
     board = game.board()
     game_key = f'{file}-{game.headers["Event"]}-{game_num}'
     blunders = []
+    previous_score = 0
     for move in game.mainline_moves():
         board.push(move)
 
-        # Get the best move
         result = engine.play(board, chess.engine.Limit(depth=10))
         best_move = result.move
 
-        # If the best move is different from the actual move
-        if best_move != move:
-            info = engine.analyse(board, chess.engine.Limit(depth=10))
-            score = info["score"].relative.score() if info["score"] else None
+        info = engine.analyse(board, chess.engine.Limit(depth=10))
+        score = info["score"].relative.score() if info["score"] else None
 
-            # If the score is not None and the difference is greater than the threshold, it's a blunder
-            if score is not None and score > blunder_threshold:
+        if score is not None:
+            if blunder_definition == "bad_move" and best_move != move and score > blunder_threshold:
                 blunders.append((board.fen(), move.uci(), best_move.uci(), score))
+            elif blunder_definition == "evaluation_drop" and score - previous_score < -blunder_threshold:
+                blunders.append((board.fen(), move.uci(), best_move.uci(), score))
+
+        previous_score = score if score is not None else previous_score
 
     engine.quit()
     return game_key, blunders
 
-def find_blunders(pgn_dir_path, stockfish_path, blunder_threshold=100):
+# change defintion of blunder
+def find_blunders(pgn_dir_path, stockfish_path, blunder_definition="bad_move", blunder_threshold=100):
     game_infos = []
 
     for root, _, files in os.walk(pgn_dir_path):
@@ -46,12 +50,11 @@ def find_blunders(pgn_dir_path, stockfish_path, blunder_threshold=100):
                     while True:
                         game = chess.pgn.read_game(pgn)
                         if game is None:
-                            break  # end of file
+                            break
 
-                        game_infos.append((stockfish_path, blunder_threshold, (file, game, game_num)))
+                        game_infos.append((stockfish_path, blunder_threshold, (file, game, game_num), blunder_definition))
                         game_num += 1
 
-    # use as many processes as there are CPUs
     with Pool(cpu_count()) as p:
         blunders = p.map(analyze_game, game_infos)
 
@@ -60,7 +63,7 @@ def find_blunders(pgn_dir_path, stockfish_path, blunder_threshold=100):
 if __name__ == '__main__':
     stockfish_path = "engines/stockfish"
     pgn_dir_path = "test_data"
-    blunders = find_blunders(pgn_dir_path, stockfish_path)
+    blunders = find_blunders(pgn_dir_path, stockfish_path, blunder_definition="evaluation_drop")
     for game, game_blunders in blunders.items():
         logging.info(f"In game {game}, there were {len(game_blunders)} blunders.")
         print(f"In game {game}, there were {len(game_blunders)} blunders.")
