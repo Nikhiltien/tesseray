1abbfe4b9893d7f134e7f38928065546bab33241
nikhiltien <nikhil17tn@gmail.com>
2023-05-18 16:38:02 -0400

    complete

--- tesseray/blunder_check.py
+++ tesseray/blunder_check.py
@@ -4,79 +4,95 @@ import os
 import multiprocessing
 from datetime import datetime
 
-def write_to_log(log_dir, blunders):
+def log_blunders(log_dir, all_blunders):
     flag = datetime.now().strftime('%Y%m%d%H%M%S')
     log_file = os.path.join(log_dir, f"blunder_check_{flag}.log")
 
-    with open(log_file, 'a') as log_f:
-        for game_header, blunders_list in blunders:
+    with open(log_file, 'w') as log_f:
+        for game_header, blunders_list in all_blunders:
             log_f.write(game_header)
             for blunder in blunders_list:
                 log_f.write(f"{blunder}\n")
             log_f.write(f"Total blunders in game: {len(blunders_list)}\n")
 
-def evaluate_game(pgn_file, stockfish_path, limit=150):
+def game_generator(pgn_file):
     with open(pgn_file) as f:
-        blunders = []
-
-        engine = chess.engine.SimpleEngine.popen_uci(stockfish_path)
-
         while True:
             game = chess.pgn.read_game(f)
             if game is None:
                 break
+            yield game
 
-            blunder_count = 0
-            game_blunders = []
-            node = game
-            game_header = f"Evaluating Game: {pgn_file}\n"
+def evaluate_game(pgn_file, stockfish_path, limit=150):
+    blunders = []
 
-            while not node.is_end():
-                next_node = node.variations[0]
-                move = next_node.move
+    engine = chess.engine.SimpleEngine.popen_uci(stockfish_path)
 
-                # Evaluate the position before the move
-                result = engine.analyse(node.board(), chess.engine.Limit(depth=10))
-                score_before = result["score"].relative.score()
-                if score_before is None:
-                    # Mate in x situation
-                    score_before = (30000 if result["score"].relative.is_mate() else 0)
+    for game in game_generator(pgn_file):
+        game_blunders = []
+        node = game
+        game_header = f"Evaluating Game: {pgn_file}\n"
 
-                # Evaluate the position after the move
-                result = engine.analyse(next_node.board(), chess.engine.Limit(depth=10))
-                score_after = result["score"].relative.score()
-                if score_after is None:
-                    # Mate in x situation
-                    score_after = (30000 if result["score"].relative.is_mate() else 0)
+        best_move = None
+        while not node.is_end():
+            next_node = node.variations[0]
+            move = next_node.move
 
-                if abs(score_before + score_after) > limit:
-                    blunder_count += 1
-                    move_number = (node.ply() + 1) // 2
-                    if node.board().turn:
-                        move_notation = f"{move_number}. {node.board().san(move)}"
-                    else:
-                        move_notation = f"{move_number}. ...{node.board().san(move)}"
-                    blunder_msg = f"A blunder occurred on move: {move_notation}"
-                    game_blunders.append(blunder_msg)
+            # Evaluate the position before the move
+            result = engine.analyse(node.board(), chess.engine.Limit(depth=10))
+            score_before = result["score"].relative.score()
+            if score_before is None:
+                # Mate in x situation
+                score_before = (30000 if result["score"].relative.is_mate() else 0)
 
-                node = next_node
+            # Find the best move before the blunder
+            if best_move is None:
+                best_move_result = engine.play(node.board(), chess.engine.Limit(depth=10))
+                best_move = best_move_result.move
 
-            blunders.append((game_header, game_blunders))
+            # Evaluate the position after the move
+            result = engine.analyse(next_node.board(), chess.engine.Limit(depth=10))
+            score_after = result["score"].relative.score()
+            if score_after is None:
+                # Mate in x situation
+                score_after = (30000 if result["score"].relative.is_mate() else 0)
 
-        engine.quit()
+            if abs(score_before + score_after) > limit:
+                move_number = (node.ply() + 1) // 2
+                if node.board().turn:
+                    move_notation = f"{move_number}. {node.board().san(move)}"
+                else:
+                    move_notation = f"{move_number}. ...{node.board().san(move)}"
+                best_move_notation = node.board().san(best_move)
+                blunder_msg = f"A blunder occurred on move: {move_notation}, best move was: {best_move_notation}"
+                game_blunders.append(blunder_msg)
+
+            node = next_node
+            best_move = None
+
+        blunders.append((game_header, game_blunders))
+
+    engine.quit()
 
     return blunders
 
+def process_new_files(previous_files, directory):
+    current_files = set(os.listdir(directory))
+    new_files = current_files.difference(previous_files)
+    return new_files
+
 if __name__ == "__main__":
     directory = 'test_data'
-    pgn_files = [os.path.join(directory, file) for file in os.listdir(directory) if file.endswith('.pgn')]
+    previous_files = set()  # load this from disk if you have it saved
+    new_pgn_files = process_new_files(previous_files, directory)
+    new_pgn_files = [os.path.join(directory, file) for file in new_pgn_files if file.endswith('.pgn')]
+
     stockfish_path = 'engines/stockfish'
     log_dir = "logs"
     os.makedirs(log_dir, exist_ok=True)
 
-    # create a process pool and start evaluation
     with multiprocessing.Pool() as pool:
-        results = pool.starmap(evaluate_game, [(pgn_file, stockfish_path) for pgn_file in pgn_files])
+        results = pool.starmap(evaluate_game, [(pgn_file, stockfish_path) for pgn_file in new_pgn_files])
 
     all_blunders = [blunder for result in results for blunder in result]
-    write_to_log(log_dir, all_blunders)
+    log_blunders(log_dir, all_blunders)
